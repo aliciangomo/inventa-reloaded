42d0eb2cab500647fff453661c4e09bcebe57127
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-11-26 00:05:27 +0000

    show for recent work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def inventa
+  end
+
+  def mypet
+  end
+
+  def aliciaart
+  end
+
+  def arthub
+  end
+
+  def pamperme
+  end
+
+  def fitfashion
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  get "arthub", to: 'pages#arthub', as: "arthub"
+  get "pamperme", to: 'pages#pamperme', as: "pamperme"
+  get "inventa", to: 'pages#inventa', as: "inventa"
+  get "fitfashion", to: 'pages#fitfashion', as: "fitfashion"
+  get "aliciaart", to: 'pages#contact', as: "aliciaart"
+  get "mypet", to: 'pages#mypet', as: "mypet"
+
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
