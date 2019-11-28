c631ce7e8475bc2f1260fbbc02ca3023409f1e26
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-11-28 15:23:25 +0000

    created error pages, configured robots.txt and added remaning project pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,4 +19,5 @@ class PagesController < ApplicationController
 
   def fitfashion
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,11 @@ Rails.application.routes.draw do
 
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unacceptable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
+  get '/sitemap.xml' => 'sitemaps#index', defaults: { format: 'xml' }
+  get "/robots.:format", to: "robots#index"
 end
